cf0d71bab512f5fead172b602b3b54021bfbed52
Arianna <stanlyfiji@gmail.com>
2022-08-25 12:03:38 +0300

    Solution

---
+++ Task41/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[] strToIntArray(string a, int N)
+{
+    a = a.Replace(" ", "");
+    string[] b = a.Split(",");
+    int[] c = new int[N] ;
+    for (int i = 0; i < b.Length & i< N; i++)
+    {
+        c[i] = Convert.ToInt32(b[i]);
+       // System.Console.WriteLine(c[i]);
+    }
+    return c;
+}
+
+
+int PositiveCounter( int[] Array)
+{
+   int counter=0;
+   for (int i = 0; i < Array.Length; i++)
+   if (Array[i]>0)
+       counter++;
+   return counter;
+}
+
+System.Console.Write("Сколько чисел будете вводить? M = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите числа через , :  ");
+
+int[] Array = new int[N];
+Array = strToIntArray(Console.ReadLine(), N);
+System.Console.WriteLine( $"Положительных чисел всего {PositiveCounter(Array)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿(double x,double y ) CrossLine(double k1, double b1, double k2, double b2)
+{   double x, y;
+    x = (b1-b2)/(k2-k1);
+    y = k1*x + b1;
+    return (x,y); 
+}
+System.Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+var Point = CrossLine(k1,b1,k2,b2);
+System.Console.WriteLine($"Точка пересечения({Point.x},{Point.y})");
